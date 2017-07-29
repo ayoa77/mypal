1169732c7674679e9e507a99c91597c48092b872
ayoa77 <ayodeleamadi@gmail.com>
2017-07-29 16:23:15 +0800

    migration fix

--- db/migrate/20150526040154_create_settings.rb
+++ db/migrate/20150526040154_create_settings.rb
@@ -3,8 +3,6 @@ class CreateSettings < ActiveRecord::Migration
     create_table :settings do |t|
       t.string :key
       t.string :value
-# 20150209161054 for culprit
-# 20150526040154
     end
   end
 end
