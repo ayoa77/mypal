314fc7eccdf8cb8a03ac0990b14518427eb43c9d
ayoa77 <ayodeleamadi@gmail.com>
2017-07-31 18:24:32 +0800

    taking migration errors out of 3001

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,8 @@ Rails.application.configure do
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
+  config.active_record.migration_error = false
+
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
