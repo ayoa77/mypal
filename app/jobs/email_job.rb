3f8236b834e01f3b405f4b79595ad6d3861bfbbb
ayoa77 <ayodeleamadi@gmail.com>
2017-08-17 20:11:48 +0800

    making changes to find out where reseque links with emailjobs

--- app/jobs/email_job.rb
+++ app/jobs/email_job.rb
@@ -4,7 +4,7 @@ class EmailJob
   @queue = :email
 
   def self.perform(from, to, subject, text, html)
-    RestClient.post "#{Rails.application.secrets.mailgun_base_url}messages",
+    RestClient.post "#{Rails.application.secrets.mailgun_base_url}/messages",
       from: from,
       to: to,
       subject: subject,
