2a474d30dab84c450a68177cf26fc348a88ecddb
ayoa77 <ayodeleamadi@gmail.com>
2017-08-18 17:05:59 +0800

    rhcp testing email config

--- app/jobs/email_job.rb
+++ app/jobs/email_job.rb
@@ -11,4 +11,18 @@ class EmailJob
       text: text,
       html: html
   end
+
+
+def self.test
+begin
+RestClient.post "#{Rails.application.secrets.mailgun_base_url}messages",
+  from: 'test <ayodeleamadi@mg.globetutoring.com>',
+  to: 'ayodeleamadi@gmail.com',
+  subject: 'subjectify',
+  text: 'textme',
+  html: 'htmlme'
+rescue RestClient::ExceptionWithResponse => e
+  puts e.response
+end
+end
 end
--- lib/custom_logger.rb
+++ lib/custom_logger.rb
@@ -2,7 +2,7 @@ class CustomLogger
 	def self.add( filename, method, parameters, error = nil )
 
     entry = { file: filename, method: method }.merge(parameters)
-    # entry = entry.merge(error: error) if error.present?
+    entry = entry.merge(error: error) if error.present?
 
     Log.add(entry)
 
