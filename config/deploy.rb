cdddfa26acd43aa5ba370adcd145a96c993b72e2
ayoa77 <ayodeleamadi@gmail.com>
2017-09-06 17:16:58 +0800

    making minor changes to text for presentation

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.4.1'
 
 set :application, 'globetutoring'
 set :repo_url, 'https://ayoa77:S6SMTfsmuF9vFRNeSy84@bitbucket.org/ayoa77/globetutoring.git'
-set :branch, "backend"
+set :branch, "frontend"
 
 set :user, "aj"
 set :rails_env, "production"
