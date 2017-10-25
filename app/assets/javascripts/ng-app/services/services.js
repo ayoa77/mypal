efb62c3929fdf6c413fc2b7b4ef5025cd00f366e
ayoa77 <ayodeleamadi@gmail.com>
2017-10-25 15:43:23 +0800

    pushing to test font size changes

--- app/assets/javascripts/ng-app/services/services.js
+++ app/assets/javascripts/ng-app/services/services.js
@@ -831,7 +831,7 @@ angular.module('blnkk.services', [ 'persona' ])
 			clientId = '7727a6dzcubr3n';
 			redirect = encodeURIComponent("http://localhost:3000/oauth2authentication");
 		} else {
-			clientId = '7727a6dzcubr3n';
+			clientId = '75grdrw2yddh6s';
 			redirect = encodeURIComponent("http://heylions.club/oauth2authentication");
 		}
 		this.getAuthenticationURL = function(redirectTo, requestType) {
