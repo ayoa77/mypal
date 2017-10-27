53104cee2dfef40f1f6fc09e01c952c3d2b108d7
ayoa77 <ayodeleamadi@gmail.com>
2017-10-27 17:54:19 +0800

    changing linkedin client id

--- app/assets/javascripts/ng-app/services/services.js
+++ app/assets/javascripts/ng-app/services/services.js
@@ -831,7 +831,7 @@ angular.module('blnkk.services', [ 'persona' ])
 			clientId = '7727a6dzcubr3n';
 			redirect = encodeURIComponent("http://localhost:3000/oauth2authentication");
 		} else {
-			clientId = '7727a6dzcubr3n';
+			clientId = '81fmq7b1l2fmnn';
 			redirect = encodeURIComponent("http://heylions.club/oauth2authentication");
 		}
 		this.getAuthenticationURL = function(redirectTo, requestType) {
