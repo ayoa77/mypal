ec609d31e3a4ef17aea72f8c19470e1753bf4f82
ayoa77 <ayodeleamadi@gmail.com>
2017-11-08 18:04:52 +0800

    logos and fronend and such

--- config/application.rb
+++ config/application.rb
@@ -43,7 +43,7 @@ module Blnkk
           '.html', '.erb', '.haml',                 # Templates
           '.png',  '.gif', '.jpg', '.jpeg',         # Images
           '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
-          '.js', '.scss', '.css', '.less'
+          '.js', 
         ]
       end)
     end
