22214fdcafe3c0e5a7ebec4e9c5c5314456a8dac
ayoa77 <ayodeleamadi@gmail.com>
2017-11-11 18:54:51 +0800

    pushing changes to fix subdomains

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -65,7 +65,10 @@ class BaseController < ActionController::Base
       ActiveRecord::Base.connection.active? #raises expection if database does not exist
     rescue
       ActiveRecord::Base.establish_connection(Rails.env.to_sym)
-      redirect_to ENV['ROOT_URL']
+      byebug
+      redirect_to request.url
+      # redirect_to ENV['ROOT_URL']
+
     end
     @websocket_prefix = ActiveRecord::Base.connection.current_database
   end
