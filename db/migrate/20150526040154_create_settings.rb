5584487d52f5201309f22cfd1c454e75f5d3e54d
ayoa77 <ayodeleamadi@gmail.com>
2017-11-23 16:15:56 +0800

    making changes to migrations to push them through

--- db/migrate/20150526040154_create_settings.rb
+++ db/migrate/20150526040154_create_settings.rb
@@ -1,10 +1,10 @@
 class CreateSettings < ActiveRecord::Migration
   def change
-    unless table_exists?(:settings)
+    # unless table_exists?(:settings)
       create_table :settings do |t|
         t.string :key
         t.string :value
-      end
+      # end
     end
   end
 end
