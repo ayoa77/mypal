d952185261c578ec10626cf8f7391e6883edaf2c
ayoa77 <ayodeleamadi@gmail.com>
2017-11-23 16:20:17 +0800

    making changes to migrations to push them through

--- db/migrate/20150604031035_add_socket_key_to_users.rb
+++ db/migrate/20150604031035_add_socket_key_to_users.rb
@@ -1,6 +1,7 @@
 class AddSocketKeyToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :socket_key, :string, after: :active_sessions unless User.column_names.include?('socket_key')
+    add_column :users, :socket_key, :string, after: :active_sessions 
+    # unless User.column_names.include?('socket_key')
     # User.all.each do |u|
     #   u.update_columns(socket_key: SecureRandom.hex)
     # end
